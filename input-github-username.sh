6578ae384c289afb3cfb707ade070757831211c0
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2022-04-21 20:58:32 +0000

    script is finalized

--- input-github-username.sh
+++ input-github-username.sh
@@ -1,4 +1,3 @@
-echo Hello World!
 set -u;
 if ! gp env | grep -q 'GITHUB_USERNAME'; then {
   until read -r -p "++++ Enter your Github username (only required once): " GITHUB_USERNAME; do continue; done
@@ -9,11 +8,11 @@ if test ! -v GITHUB_USERNAME; then {
   eval $(gp env -e);
 } fi
 
-echo remote url script
+git -C /workspace/mattermost-gitpod-config remote add upstream https://github.com/mattermost/mattermost-gitpod-config.git
+git -C /workspace/mattermost-gitpod-config remote set-url origin https://github.com/${GITHUB_USERNAME}/mattermost-gitpod-config.git
+
 git -C /workspace/mattermost-server remote add upstream https://github.com/mattermost/mattermost-server.git
 git -C /workspace/mattermost-server remote set-url origin https://github.com/${GITHUB_USERNAME}/mattermost-server.git
 
 git -C /workspace/mattermost-webapp remote add upstream https://github.com/mattermost/mattermost-webapp.git
 git -C /workspace/mattermost-webapp remote set-url origin https://github.com/${GITHUB_USERNAME}/mattermost-webapp.git
-
-echo remote url script completed
