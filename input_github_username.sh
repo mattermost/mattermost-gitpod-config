e2b461745eec2e5b550826f44bee8422965d00f9
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2022-04-21 20:15:43 +0000

    github username script is added

---
+++ input_github_username.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo Hello World!
+set -u;
+if ! gp env | grep -q 'GITHUB_USERNAME'; then {
+  until read -r -p "++++ Enter your Github username (only required once): " GITHUB_USERNAME; do continue; done
+  gp env GITHUB_USERNAME="$GITHUB_USERNAME"; # Sets this variable on https://gitpod.io/variables which can be used later on
+} fi
+
+if test ! -v GITHUB_USERNAME; then {
+  eval $(gp env -e);
+} fi
