4f902a224ce0d2ed2bf52304aa5dd2c69d6b766d
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2022-04-21 20:50:25 +0000

    set remote script is moved to same file

--- input-github-username.sh
+++ input-github-username.sh
@@ -8,3 +8,12 @@ if ! gp env | grep -q 'GITHUB_USERNAME'; then {
 if test ! -v GITHUB_USERNAME; then {
   eval $(gp env -e);
 } fi
+
+echo remote url script
+git -C /workspace/mattermost-server remote add upstream https://github.com/mattermost/mattermost-server.git
+git -C /workspace/mattermost-server remote set-url origin https://github.com/${GITHUB_USERNAME}/mattermost-server.git
+
+git -C /workspace/mattermost-webapp remote add upstream https://github.com/mattermost/mattermost-webapp.git
+git -C /workspace/mattermost-webapp remote set-url origin https://github.com/${GITHUB_USERNAME}/mattermost-webapp.git
+
+echo remote url script completed
--- set-origin-remotes-to-forks.sh
+++ set-origin-remotes-to-forks.sh
@@ -1,8 +1,8 @@
-echo remote url script
-git -C /workspace/mattermost-server remote add upstream https://github.com/mattermost/mattermost-server.git
-git -C /workspace/mattermost-server remote set-url origin https://github.com/${GITHUB_USERNAME}/mattermost-server.git
+# echo remote url script
+# git -C /workspace/mattermost-server remote add upstream https://github.com/mattermost/mattermost-server.git
+# git -C /workspace/mattermost-server remote set-url origin https://github.com/${GITHUB_USERNAME}/mattermost-server.git
 
-git -C /workspace/mattermost-webapp remote add upstream https://github.com/mattermost/mattermost-webapp.git
-git -C /workspace/mattermost-webapp remote set-url origin https://github.com/${GITHUB_USERNAME}/mattermost-webapp.git
+# git -C /workspace/mattermost-webapp remote add upstream https://github.com/mattermost/mattermost-webapp.git
+# git -C /workspace/mattermost-webapp remote set-url origin https://github.com/${GITHUB_USERNAME}/mattermost-webapp.git
 
-echo remote url script completed
+# echo remote url script completed
