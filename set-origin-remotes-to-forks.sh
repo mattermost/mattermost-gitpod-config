43e412c2e3569f285476fd3aef11fabf51a39587
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2022-04-24 16:31:48 +0000

    unnecessary echo is deleted

--- set-origin-remotes-to-forks.sh
+++ set-origin-remotes-to-forks.sh
@@ -1,4 +1,3 @@
-echo GITHUB_USERNAME
 git -C /workspace/mattermost-server remote add upstream https://github.com/mattermost/mattermost-server.git
 git -C /workspace/mattermost-server remote set-url origin https://github.com/${GITHUB_USERNAME}/mattermost-server.git
 
